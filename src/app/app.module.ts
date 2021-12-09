8dcf1bd5fa0065e4eedb77913d564f58cbc6dd56
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2021-12-08 20:59:59 -0600

    added CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ModalModule } from 'ngx-bootstrap/Modal';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-list',
