7dd4c9dc6a4e4976e191c1a50c7ac10ca2bfdc5a
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2022-02-21 13:27:37 -0600

    updated server,js and package.json
    added function requireHTTS in server.js
    added  "start": "node server.js",
    "build": "ng build --prod",  in package.json

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-
+app.use(requireHTTPS);
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/MyTrip'));
+app.use(express.static('./dist/my-trip'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/MyTrip/'}),
+app.get('/*', function(req, res){
+    res.sendFile('index.html', {root: 'dist/my-trip/'},
 );
-
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
